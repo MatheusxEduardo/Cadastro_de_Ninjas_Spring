221ffe60b265cf34eb5f5f7a6ae9e4ff46b0bff8
Matheus Eduardo <matheusxeduardox@outlook.com>
2025-09-02 22:28:02 -0300

    feat: Create controller rots with mock

--- src/main/java/dev/MatheusJava/CadastroDeNinjas/Missoes/MissoesController.java
+++ src/main/java/dev/MatheusJava/CadastroDeNinjas/Missoes/MissoesController.java
@@ -1,4 +1,28 @@
 package dev.MatheusJava.CadastroDeNinjas.Missoes;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("missoes")
 public class MissoesController {
+    
+    @GetMapping("/listar")
+    public String listarMissao() {
+        return "MIssão listada com sucesso.";
+    }
+
+    @PostMapping("/criar")
+    public String criarMissao() {
+        return "Missão criada com sucesso.";
+    }
+
+    @PutMapping("/alterar")
+    public String alterarMissao() {
+        return "Missão alterada com sucesso.";
+    }
+
+    @DeleteMapping("/deletar")
+    public String deletarMissao() {
+        return "Missão deletada com sucesso.";
+    }
 }
