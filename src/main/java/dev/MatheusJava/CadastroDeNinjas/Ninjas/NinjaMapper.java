e308250e88bd485c83f9abde1e08b1bfaeb426ae
Matheus Eduardo <matheusxeduardox@outlook.com>
2025-11-11 22:55:29 -0300

    feat(mapper): implementa conversão entre Ninja e NinjaDto

--- src/main/java/dev/MatheusJava/CadastroDeNinjas/Ninjas/NinjaMapper.java
+++ src/main/java/dev/MatheusJava/CadastroDeNinjas/Ninjas/NinjaMapper.java
@@ -1,4 +1,32 @@
 package dev.MatheusJava.CadastroDeNinjas.Ninjas;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class NinjaMapper {
+    public NinjaModel map(NinjaDTO ninjaDTO) {
+        NinjaModel ninjaModel = new NinjaModel();
+        ninjaModel.setId(ninjaDTO.getId());
+        ninjaModel.setNome(ninjaDTO.getNome());
+        ninjaModel.setEmail(ninjaDTO.getEmail());
+        ninjaModel.setIdade(ninjaDTO.getIdade());
+        ninjaModel.setImgUrl(ninjaDTO.getImgUrl());
+        ninjaModel.setMissoes((ninjaDTO.getMissoes()));
+        ninjaModel.setRank(ninjaDTO.getRank());
+
+        return ninjaModel;
+    }
+
+    public NinjaDTO map(NinjaModel ninjaModel){
+        NinjaDTO ninjaDTO = new NinjaDTO();
+        ninjaDTO.setId(ninjaModel.getId());
+        ninjaDTO.setNome(ninjaModel.getNome());
+        ninjaDTO.setEmail(ninjaModel.getEmail());
+        ninjaDTO.setIdade(ninjaModel.getIdade());
+        ninjaDTO.setImgUrl(ninjaModel.getImgUrl());
+        ninjaDTO.setMissoes(ninjaModel.getMissoes());
+        ninjaDTO.setRank(ninjaModel.getRank());
+
+        return ninjaDTO;
+    }
 }
