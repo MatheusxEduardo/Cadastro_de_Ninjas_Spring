08c2b81860f9a3b389bc8e5c57c1644d706b8de5
Matheus Eduardo <matheusxeduardox@outlook.com>
2025-11-11 22:55:12 -0300

    feat(dto): cria estrutura DTO para entidade Ninja

--- src/main/java/dev/MatheusJava/CadastroDeNinjas/Ninjas/NinjaDTO.java
+++ src/main/java/dev/MatheusJava/CadastroDeNinjas/Ninjas/NinjaDTO.java
@@ -1,4 +1,21 @@
 package dev.MatheusJava.CadastroDeNinjas.Ninjas;
 
+import dev.MatheusJava.CadastroDeNinjas.Missoes.MissoesModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class NinjaDTO {
+
+    private Long id;
+    private String nome;
+    private String email;
+    private String imgUrl;
+    private int idade;
+    private MissoesModel missoes;
+    private String rank;
 }
